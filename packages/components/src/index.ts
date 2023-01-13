fcced481ea19496c43d937df675e556bf115cd19
Heddi <heddi932@gmail.com>
2023-01-14 00:07:33 +0100

    feat(component): add container component

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,5 +1,6 @@
 import Html from './Html.vue';
 import Body from './Body.vue';
 import Link from './Link.vue';
+import Container from './Container.vue';
 
-export { Html, Body, Link };
+export { Html, Body, Link, Container };
