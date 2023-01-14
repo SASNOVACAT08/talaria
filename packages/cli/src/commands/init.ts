14ab869121a92f53c625ae476855e95cf88ba07a
Ruben Barros <atosc08@gmail.com>
2023-01-14 18:30:21 +0100

    feat(cli): init command ready

--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -1,5 +1,14 @@
 import { Argv, ArgumentsCamelCase } from 'yargs';
+import spinner from '../utils/spinner';
+import { cp } from 'fs';
 import chalk from 'chalk';
+import path from 'path';
+import { fileURLToPath, pathToFileURL } from 'url';
+
+interface InitArgv {
+  name: string;
+  template: string;
+}
 
 export default class Init {
   static command = 'init <name> [template]';
@@ -15,16 +24,27 @@ export default class Init {
         alias: 't',
         describe: 'Template to use',
         type: 'string',
+        default: 'simple',
       });
   };
 
-  static handler = (argv: ArgumentsCamelCase) => {
-    console.log(
-      chalk.green(
-        `Creating project ${chalk.yellow(argv.name)} with template ${
-          argv.template
-        }`
-      )
+  static handler = (argv: ArgumentsCamelCase<InitArgv>) => {
+    spinner.start('Initializing project');
+    const cliPath = path.resolve(
+      fileURLToPath(pathToFileURL(__filename).toString()),
+      '../..',
+      'templates'
     );
+    const templatePath = path.join(cliPath, argv.template);
+    cp(templatePath, argv.name, { recursive: true }, (err) => {
+      if (err) return this.error();
+      spinner.stop();
+      console.log(chalk.green('Project initialized successfully!'));
+    });
+  };
+
+  static error = () => {
+    spinner.stop();
+    console.error(chalk.red('Error building project'));
   };
 }
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -4,8 +4,6 @@ import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
 import Init from './commands/init';
-import Add from './commands/add';
-import Dev from './commands/dev';
 import Build from './commands/build';
 import Send from './commands/send';
 
@@ -17,8 +15,6 @@ yargs(hideBin(process.argv))
     )
   )
   .command(Init.command, Init.description, Init.builder, Init.handler)
-  .command(Add.command, Add.description, Add.builder, Add.handler)
-  .command(Dev.command, Dev.description, Dev.builder, Dev.handler)
   .command(Build.command, Build.description, Build.builder, Build.handler)
   .command(Send.command, Send.description, Send.builder, Send.handler)
   .help().argv;
