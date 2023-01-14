501214532e7d8d6cf4ec6ed9296521a52a01fb90
Heddi <heddi932@gmail.com>
2023-01-14 01:06:27 +0100

    feat(hr): added hr component with tests

---
+++ packages/components/src/Hr.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps({
+  props: {
+    type: Object,
+    default: () => ({}),
+  },
+  style: {
+    type: Object,
+    default: () => ({}),
+  },
+});
+</script>
+
+<template>
+  <hr
+    ref="forwardedRef"
+    :style="{
+      width: '100%',
+      border: 'none',
+      borderTop: '1px solid #eaeaea',
+      ...style,
+    }"
+    :v-bind="props"
+  />
+</template>
