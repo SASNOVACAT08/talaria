7791cc0f1d831eb44c8a8c4967710accc7f80e02
Heddi <heddi932@gmail.com>
2023-01-15 18:34:23 +0100

    fix(preview): fix named import

--- test/components/test/preview.test.ts
+++ test/components/test/preview.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { Preview } from '@talaria/components';
+import { Preview } from '@tlra/components';
 import { mount } from '@vue/test-utils';
 
 describe('Preview.vue', () => {
